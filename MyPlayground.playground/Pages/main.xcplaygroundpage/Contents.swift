568bac4aeaf59123b556d4d52209553b56376bcc
JimCampagno <jimcampagno@gmail.com>
2016-05-12 13:22:09 -0400

    create outline

---
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*: Outline
+ 
+ 
+ # Functions
+ 
+ ### Readings associated with this lab
+ 
+ * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
+
+ */
+/*: question1
+ ### 1. Question making the student think of how best to structure a function (or this first one can be VERY simple in just having them get used to the syntax of typing a function)
+ */
+// write your code here
+
+
+
+
+/*: question2
+ ### 2. Another question about functions.. weeee
+ */
+// write your code here
+
+
+
+
+/*: question3
+ ### 3. Moar questions...
+ */
+// write your code here
+
+
+/*: question4
+ ### 4. Function function function
+ */
+// write your code here
+
+
+
+
+/*:
+ [Solution](solution)
+ */
+// ❤️
---
+++ MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+//: [Go Back](@next)
+// 1
+
+print("This is the solution. :) ")
+
+
+
+// 2
+
+let name: String
+
+
+
+
+
+
+
+// 3
+
+var test = "test"
+
+
+
+
+
+// 4
+
+var moreFakeData = "HELLO!!"    
