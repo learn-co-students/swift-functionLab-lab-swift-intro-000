1904a469eb418b5738b61a59587e76e184fb243b
Simone Grant <simonegrant@C4Qs-MBP-96.fios-router.home>
2016-09-16 21:13:45 -0400

    Functions Update

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,7 +12,9 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
-
+func helloWorld() {
+    print("Hello, world!")
+}
 
 
 
@@ -20,14 +22,23 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myFunc() {
+    let myString = "Hello!"
+    print(myString)
+}
 
-
+myFunc()
 
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func greeting(name: String) {
+    print("Hello \(name)!")
+}
+
+greeting(name: "Matt")
 
 
 
@@ -37,16 +48,20 @@
  */
 // write your code here
 
-
+var name = "Lucy"
+greeting(name: name)
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func myVar() {
+    let num = 5
+    print("There are \(num) birds on the ledge.")
+}
 
-
-
+myVar()
 
 /*:
 Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions. */
