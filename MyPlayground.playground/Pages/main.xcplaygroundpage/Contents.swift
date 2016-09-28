6998da8d182c836bc542e69d01d4748f18eb9e5d
JoumanaE <joumanaelalaoui@JoumanamanasAir.home>
2016-09-28 16:40:25 -0400

    Functions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -8,18 +8,22 @@
  * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
 
  */
-/*: question1
- ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
- */
-// write your code here
 
-
-
+func HelloWorld(name: String) {
+    print ("Hello \(name)")
+}
+HelloWorld(name: "World")
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
-// write your code here
+func Hello(name :String) {
+     print("Hello \(name)")
+    
+}
+
+Hello(name: "Lucie")
+
 
 
 
@@ -27,7 +31,16 @@
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
-// write your code here
+func Hello2(name :String) {
+    print("Hello \(name)")
+    
+}
+
+Hello2(name: "David")
+Hello2(name: "Alain")
+Hello2(name: "Nadia")
+
+
 
 
 
@@ -35,7 +48,20 @@
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
-// write your code here
+func Hello3() {
+    var firstName = "Sebaestien"
+    print("Hello \(firstName)")
+}
+
+Hello3()
+var firstName = "Laurel"
+Hello3()
+
+
+// I see that my variable did not change- It has probably something to do with the scoping
+
+
+
 
 
 
@@ -43,7 +69,30 @@
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
-// write your code here
+func Hello4() {
+    var first = "Laurel"
+    print("Hello \(first)")
+}
+
+Hello4()
+
+
+//As long as it is not in the function's scope, the var or let won't change 
+
+func Hello5(fname: String) {
+    print("Hello \(fname)")
+}
+
+Hello5(fname: "Dan")
+Hello5(fname: "Rebecca")
+
+
+
+
+
+
+
+
 
 
 
