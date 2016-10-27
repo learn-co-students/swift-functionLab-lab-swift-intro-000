5cbc289832bb9a8d18ff1af08a7b4f5f6d453cbe
kasiarosenb <kasiarosenb@gmail.com>
2016-10-27 11:04:09 -0400

    functions tasks

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,7 +11,10 @@
 /*: question1
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
-// write your code here
+func helloWorld() {
+    print("Hello World")
+}
+helloWorld()
 
 
 
@@ -19,7 +22,10 @@
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
-// write your code here
+func callName(name: String) {
+    print("hi! My name is \(name)")
+}
+callName(name: "Kasia")
 
 
 
@@ -27,8 +33,12 @@
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
-// write your code here
-
+func myname(name: String) {
+    print("hello \(name)")
+}
+myname(name: "bob")
+myname(name: "Scott")
+myname(name: "boo")
 
 
 
@@ -36,14 +46,22 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
+var name = "bob"
+myname(name: name)
+name = "jett"
+myname(name: name)
 
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
-// write your code here
+func mydog(){
+    let dogname = "amber"
+    print("my dog's name is \(dogname)")
+}
+mydog()
+
 
 
 
