40d5e9e2d19a03e6d3bd7286f4680edda77e8e8e
Sean Acres <shakers@whisper.sh>
2016-12-05 18:10:48 -0800

    Completed lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,30 +13,43 @@
  */
 // write your code here
 
+func helloWorld() {
+    print("Hello, world!")
+}
 
-
-
+helloWorld()
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
 
+func printConstant() {
+    let constantPrinted = "I'm a constant."
+    print(constantPrinted)
+}
 
-
-
+printConstant()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func printGreeting(whoToGreet:String) {
+    print("Hello \(whoToGreet)")
+}
 
-
-
+printGreeting(whoToGreet: "Jeff")
+printGreeting(whoToGreet: "Sarah")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+var whoToGreet = "Joe"
 
+printGreeting(whoToGreet: whoToGreet)
+
+whoToGreet = "Sam"
+printGreeting(whoToGreet: whoToGreet)
 
 
 
@@ -45,8 +58,14 @@
  */
 // write your code here
 
+func printVariable() {
+    var randomVariable = 5
+    print(randomVariable)
+    randomVariable = 10
+    print(randomVariable)
+}
 
-
-
+printVariable()
+printVariable()
 /*:
 Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions. */
