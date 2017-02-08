20c71e311ad8f1bafa86122cdb539ae36e742e23
Justin E. Green <justin.emir.green@gmail.com>
2017-02-07 20:15:05 -0600

    Completed Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,30 +14,39 @@
 // write your code here
 
 
+func helloWorld() {
+    print("Hello, world!")
+}
 
-
+helloWorld()
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
 
+func constantFunct() {
+    let phrase = "Super. Awesome. Stuff"
+    print(phrase)
+}
 
-
-
+constantFunct()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
+func greeting(name: String) {
+    print("Hi, \(name)")
+}
 
-
-
+greeting(name: "Justin")
+greeting(name: "Solitaire")
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
-
+// greeting(name: hi) This generates an error in the console because the method is anticipating that a String will be passed.
 
 
 /*: question5
@@ -45,7 +54,12 @@
  */
 // write your code here
 
+func awesomeThing() {
+    var day = "Tuesday"
+    print(day)
+    day = "Friday"
+    print(day)
+}
 
-
-
+awesomeThing()
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
