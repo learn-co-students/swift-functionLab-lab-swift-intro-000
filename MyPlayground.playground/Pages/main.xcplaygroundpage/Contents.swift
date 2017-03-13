e0af292fe703f3326785a5a8dd3df11fe6185f06
Khalifa Esha <dev.khaesha@gmail.com>
2017-03-14 01:13:23 +0700

    Solve problem

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,31 +12,41 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    print("Hello, world!")
+}
 
-
+helloWorld()
 
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myFunction() {
+    let myText = "Hello world"
+    print(myText)
+}
 
-
-
+myFunction()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func sayName(name: String) {
+    print("My name is \(name)")
+}
 
-
-
+sayName(name: "John")
+sayName(name: "Wick")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
+var newName = "John Constantine"
+sayName(name: newName)
 
 
 
@@ -44,7 +54,16 @@
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func anotherFunction() {
+    var any = "LoL"
+    print(any)
+    
+    any = "LoL again"
+    print(any)
+}
 
+anotherFunction()
+anotherFunction()
 
 
 
