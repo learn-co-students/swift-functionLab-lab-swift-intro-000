98d15bb569c59d1979a6ce15eea97c2493f8ad22
ArtemisD <dianah456@gmail.com>
2017-03-27 21:27:23 -0700

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,14 +13,23 @@
  */
 // write your code here
 
+func sayHello() {
+    print("Hello, World!")
 
-
+}
+sayHello()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
 
+func constant() {
+    let greeting = "Hello, Universe!"
+    print(greeting)
+}
+
+constant()
 
 
 
@@ -31,20 +40,39 @@
 
 
 
+func greeting(name: String) {
+    print("Hello, \(name)!")
+}
+
+greeting(name: "Apple")
+greeting(name: "Blue Ivy")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
+var person1 = "Artemis"
+var person2 = "Buzz"
 
+greeting(name: person1)
+greeting(name: person2)
+person2 = "Woody"
+greeting(name: person2)
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+var name = "Becca"
+func sayGoodbye(name: String) {
+    print("Goodbye, \(name).")
+}
 
+sayGoodbye(name: name)
+name = "Kat"
+sayGoodbye(name: name)
 
 
 
