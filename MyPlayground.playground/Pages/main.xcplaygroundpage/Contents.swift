619caafb619706a85998192bee9e73526af589e6
Kristof Buts <kr.buts@gmail.com>
2017-04-11 12:55:01 +0200

    Solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,7 +12,12 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func hw() {
+    let hw = "Hello, world!"
+    print(hw)
+}
 
+hw()
 
 
 
@@ -20,7 +25,12 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func cte() {
+    let cte = 5
+    print(cte)
+}
 
+cte()
 
 
 
@@ -28,7 +38,13 @@
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func sayHello(name: String) {
+    let str = "Hello, \(name)!"
+    print(str)
+}
 
+sayHello(name: "joske")
+sayHello(name: "marieke")
 
 
 
@@ -36,15 +52,25 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
-
+var name = "jozef"
+sayHello(name: name)
+name = "fons"
+sayHello(name: name)
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func r(n: Int) {
+    var nn = n
+    print(nn)
+    nn += 1
+    print(nn)
+}
 
+r(n: 5)
+r(n: 3)
 
 
 
