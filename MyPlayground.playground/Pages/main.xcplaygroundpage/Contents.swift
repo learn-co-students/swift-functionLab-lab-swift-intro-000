000a6ccd521e8888dad8ba8f19f9ac5fd5a28892
Joshua Payne <joshua.payne@travelok.com>
2017-04-12 09:52:42 -0500

    Lab solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,7 +12,11 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    print("Hello, world!")
+}
 
+helloWorld()
 
 
 
@@ -20,7 +24,12 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func printConstant() {
+    let myConstant = "Hello, world!"
+    print(myConstant)
+}
 
+printConstant()
 
 
 
@@ -28,6 +37,12 @@
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func greet(name: String) {
+    print("Hello \(name)!")
+}
+
+greet(name: "Miles")
+greet(name: "Jenn")
 
 
 
@@ -37,13 +52,29 @@
  */
 // write your code here
 
+let name = "Josh"
+greet(name: name)
 
+var anotherName = "Luna"
+greet(name: anotherName)
+anotherName = "Charlie"
+greet(name: anotherName)
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func helloThere() {
+    var greeting = "Hello there."
+    print(greeting)
+    greeting = "Hello world"
+    print(greeting)
+}
+
+helloThere()
+helloThere()
+helloThere()
 
 
 
