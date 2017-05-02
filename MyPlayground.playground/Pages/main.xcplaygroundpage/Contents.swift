b0cb65a2744c0ca5bfe1e8b8caa8c67336bd5d98
Panagiotis <pskarl@gmail.com>
2017-05-02 20:21:13 +0300

    Done.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,7 +12,10 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
-
+func helloWorld(){
+    print("Hello World!")
+}
+helloWorld()
 
 
 
@@ -20,6 +23,11 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myFunction() {
+    let myConst: Int = 37
+    print(myConst)
+}
+myFunction()
 
 
 
@@ -28,7 +36,13 @@
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
-
+func greet(name: String) {
+    print("Hello \(name)!")
+    
+}
+greet(name: "Panos")
+greet(name: "Maria")
+greet(name: "Baby!!!")
 
 
 
@@ -36,7 +50,8 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
+var myName: String = "Skarlas"
+greet(name: myName)
 
 
 
@@ -45,7 +60,13 @@
  */
 // write your code here
 
+func myCustomFunction(){
+    var myPhrase: String = "How cool is that?"
+    print(myPhrase)
+    myPhrase = "It is soooo cool!"
+    print(myPhrase)
+}
 
-
-
+myCustomFunction()
+myCustomFunction()
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
