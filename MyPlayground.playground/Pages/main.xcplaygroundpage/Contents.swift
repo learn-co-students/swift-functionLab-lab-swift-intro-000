268af32163cb5032d327fd53667625d5507cc2a0
TeaDoan <TeaDoan@github.com>
2017-10-08 13:51:57 -0700

    completes function assignment

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,14 +12,21 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
-
-
+func helloWorld(){
+    print("Hello, world")
+}
+helloWorld()
 
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myname(){
+    let tea = "My name is Thao"
+    print(tea )
+}
+myname()
 
 
 
@@ -28,6 +35,13 @@
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func whatYourName(name:String){
+    print("My name is \(name)")
+}
+whatYourName(name: "Peter")
+whatYourName(name: "Ana")
+whatYourName(name: "Alex")
+
 
 
 
@@ -36,6 +50,32 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+let nameA = "Jennifer"
+
+whatYourName(name: nameA)
+
+var nameB = "Tiffany"
+
+whatYourName(name: nameB)
+
+nameB = "Matthew"
+
+whatYourName(name: nameB)
+
+nameB = "Lana"
+
+whatYourName(name: nameB)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -45,8 +85,22 @@
  */
 // write your code here
 
+func groupScore()
+{
+    let score1 = "A"
+    print( "Tiffany has \(score1) this semester")
+    
+    let score2 = "B"
+    print("Max has\(score2) this semester")
+    
+    
+}
+groupScore()
+groupScore()
+
+
+
 
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
-// This is a test 
