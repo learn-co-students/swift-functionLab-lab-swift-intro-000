d8c75aaba93e343bd5c189346bbf987d0ead9c26
Dane Wallace <danewallace@icloud.com>
2017-11-05 18:26:15 -0500

    Answered questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,33 +11,49 @@
 /*: question1
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
-// write your code here
 
+func helloWorld() {
+    print("Hello, world!")
+}
 
-
+helloWorld()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
 
+func myConstant() {
+    let constant = "My constant"
+    print(constant)
+}
 
-
+myConstant()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
+func printName(name: String) {
+    print("Printing \(name).")
+}
 
-
+printName(name: "Dane")
+printName(name: "Ida")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
+let myName = "Dane"
+printName(name: myName)
 
+var wifesName = "Ida"
+printName(name: wifesName)
+
+//Same behavior with string variable as string literal.
 
 
 /*: question5
@@ -45,7 +61,15 @@
  */
 // write your code here
 
+func printVariable() {
+    var variable = "Original variable."
+    print(variable)
+    variable = "Changed variable."
+    print(variable)
+}
 
+printVariable()
 
+//Same variable prints twice with both variations each time printVariable function is called.
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
