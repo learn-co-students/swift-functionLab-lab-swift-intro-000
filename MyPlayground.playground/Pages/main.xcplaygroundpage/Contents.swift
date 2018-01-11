b75ac39992a433e34955ae30e994bffc0e7e77d9
Hashiba <fhashiba@gmail.com>
2018-01-11 05:21:30 -0300

    my solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,15 +13,23 @@
  */
 // write your code here
 
-
-
+func helloWorld(){
+    
+    print("Hello, world!")
+}
+helloWorld()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myOwnFunc(){
+    let test = "Hi"
+    print(test)
+    
+}
 
-
+myOwnFunc()
 
 
 /*: question3
@@ -29,23 +37,39 @@
  */
 // write your code here
 
+func greeting(name: String){
+    
+    print("Hello \(name), nice to meet you!")
+}
 
-
-
+greeting(name: "Bob")
+greeting(name: "JC")
+greeting(name: "Mike")
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
 
+var strangerName = "11"
 
 
+greeting(name: strangerName)
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
 
+func myWay(){
+    var random = 13
+    print(random)
+    random = random+1
+    print(random)
+    
+}
 
-
+myWay()
+myWay()
+myWay()
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
