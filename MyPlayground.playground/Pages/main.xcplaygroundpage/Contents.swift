2409bbea0f3fbc25f2357ba283452e14768eaf3c
Kun Huang <kkun.huang@gmail.com>
2018-03-21 22:47:14 -0400

    functions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,31 +12,39 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
-
-
-
+func helloWorld() {
+    print("Hello, world! Where are you?")
+}
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
-
-
-
+func future(){
+    let myFuture = "What will I become?"
+    print(myFuture)
+}
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func callName(name: String) {
+    print("Hi \(name), you need to run around naked")
+}
 
-
-
+callName(name: "Nacy")
+callName(name: "Siri")
+callName(name: "Who yu")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
+var nameToCall = "New Name"
+callName(name: nameToCall)
+nameToCall = "My Hero"
+callName(name: nameToCall)
 
 
 
@@ -45,7 +53,16 @@
  */
 // write your code here
 
+func change() {
+    var num = 10
+    print(num)
+    num += 10
+    print(num)
+    num += 100
+    print(num)
+}
 
-
-
+change()
+change()
+change()
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
