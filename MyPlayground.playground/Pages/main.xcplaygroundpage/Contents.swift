2810d1cf44931634a6e09fdf09191e2f41a48e73
TheRaphaTorres <raphaelgtorres@gmail.com>
2018-03-23 19:20:51 -0300

    Completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,8 +12,10 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
-
-
+func helloWorld(){
+    print("Hello, world!")
+}
+helloWorld()
 
 
 /*: question2
@@ -21,14 +23,21 @@
  */
 // write your code here
 
-
-
+func testConstant(){
+    let nome :String = "Raphael"
+    print ("\(nome)")
+}
+testConstant()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
-
+func greeting(nome:String){
+    print("Hi \(nome)")
+}
+greeting(nome:"Raphael")
+greeting(nome:"Renata")
 
 
 
@@ -36,7 +45,10 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
-
+let nome1:String="Raphael"
+var nome2:String="Renata"
+greeting(nome:nome1)
+greeting(nome:nome2)
 
 
 
@@ -44,8 +56,16 @@
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
-
-
+func exerc5(){
+    var variable1 = "Teste"
+    print (variable1)
+    variable1="Teste2"
+    print (variable1)
+}
+exerc5()
+exerc5()
+exerc5()
+exerc5()
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-functionLab-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
