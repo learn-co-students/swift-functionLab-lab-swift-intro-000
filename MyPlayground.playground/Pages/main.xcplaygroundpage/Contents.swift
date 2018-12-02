bea9bc28c7bbb6fd169ae6040b56e7f62d55eb02
Shankar Bellam <srigirishankar@gmail.com>
2018-12-02 13:35:34 -0600

    functions lab solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,7 +12,11 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func hellowWorld(){
+    "Hello, world!"
+}
 
+hellowWorld()
 
 
 
@@ -20,23 +24,39 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func consFunc(){
+    let cons = "What?"
+    print(cons)
+    
+}
 
-
+consFunc()
 
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func personName(name: String){
+    print("Hello \(name)")
+}
 
-
+personName(name: "John Doe")
+personName(name: "Jesus Christ")
+personName(name: "Mary Jane")
 
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+let test1 = "Who"
+personName(name: test1)
 
+var test2 = "Why"
+personName(name: test2)
+test2 = "Changed"
+personName(name: test2)
 
 
 
@@ -44,7 +64,16 @@
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign a new value to this variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func variableFunc(){
+    var test3 = 3.0
+    print(test3)
+    test3 = 2.0
+    print(test3)
+}
 
+variableFunc()
+variableFunc()
+variableFunc()
 
 
 
